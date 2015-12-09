d2a5ad68cdf87fb6f33a56dba5b5c5371734fece
Katrina Guyduy <katrinaguyduy@gmail.com>
2015-12-09 15:18:10 -0800

    renamed theme

---
+++ wp-content/themes/chris-ulmer-portfolio-theme/header.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+    <!DOCTYPE html>
+    <html <?php language_attributes(); ?>>
+
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+        <?php wp_head(); ?>
+    </head>
+
+    <body <?php body_class(); ?>>
+        <div id="page" class="hfeed site">
+            <a class="skip-link screen-reader-text" href="#content">
+                <?php esc_html( 'Skip to content' ); ?>
+            </a>
+
+            <header id="masthead" class="site-header" role="banner">
+
+                <nav id="site-navigation" class="main-navigation" role="navigation">
+                    <a href="<?php echo esc_url(home_url( '/')); ?>" rel="home"><h4 class="header-name">Chris Ulmer</h4></a>
+                    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+                        <?php esc_html( 'Primary Menu' ); ?>
+                    </button>
+                    <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+                </nav>
+                <!-- #site-navigation -->
+
+
+            </header>
+            <!-- #masthead -->
+
+            <div id="content" class="site-content">
